a1465f4984e974a4e7a418506f0ff8d4ee2f80fb
Rakesh <rakesh374@outlook.com>
2024-07-16 14:09:56 +0530

    Update Education.jsx

--- src/components/sections/Education.jsx
+++ src/components/sections/Education.jsx
@@ -8,7 +8,7 @@ const Education = () => {
         <h2 className='text-5xl'>EDUCATION</h2>
         <h3>Academic Background</h3>
       </div>
-      <p>Bachelor of Computer Application Techo India Institute of Technology, Year of Graduation 2024</p>
+      <p>Bachelor of Computer Application Techno India Institute of Technology, Year of Graduation 2024</p>
       </div>
     </section>
   );
